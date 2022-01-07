cb1ee428df0c7980b8e4e6fe817aaab2e6716959
Vulpesx <potaytochipgamer@gmail.com>
2022-01-07 15:56:43 +1100

    completed chapter 16 --concurency

--- rust_book/16/p2/src/main.rs
+++ rust_book/16/p2/src/main.rs
@@ -4,6 +4,8 @@ use std::{sync::mpsc, thread, io::Seek, time::Duration};
 fn main() {
     let (tx,rx) = mpsc::channel();
 
+    let tx1 = tx.clone();
+
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
@@ -11,6 +13,22 @@ fn main() {
             String::from("the"),
             String::from("thread"),
         ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+
+            String::from("more"),
+            String::from("messaged"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
         for val in vals {
             tx.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
